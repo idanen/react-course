e31db48b9519329ba11f7f1b7c83716ba5432b77
Boris Dinkevich <boris@dinkevich.com>
2017-01-02 14:12:31 +0200

    Added iteration

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-  return React.createElement('h1', null, 'Hello!');
-}
+const recipes = ['Omelette', 'Pancake', 'Waffels'];
+
+const Header = () => (
+  <h1>Our Recipes</h1>
+);
+
+const Recipes = () => (
+  <ul>
+    { recipes.map(recipe => <li key={ recipe }>{ recipe }</li>) }
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <Header />
+    <Recipes />
+  </div>
+);
+
+
+
+window.App = App;
 
 ReactDOM.render(
-  React.createElement(App),
+  <App />,
   document.getElementById('root')
 );
